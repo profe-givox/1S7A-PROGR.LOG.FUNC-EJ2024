0ac14567ec29fdcfb7c2869e039907213682c0aa
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-19 09:56:48 -0600

    avance: obtenemos una lista de maybe Frame se agtegoina funcion que recupera el puntaje de un frame

--- HASKELL/bowling/Bowling.hs
+++ HASKELL/bowling/Bowling.hs
@@ -17,7 +17,29 @@ data Frame = Open Int Int
 --     show (Spare x y) = "Spare " ++ show x ++ " " ++ show y
 --     show (Strike x y) = "Strike " ++ show x ++ " " ++ show y
 
-toFrames :: [Int] -> [Frame]
-toFrames (x:y:ys) = Open x y : toFrames ys
-toFrames [] = []
 
+
+toFrames :: [Int] -> Maybe [Frame]
+toFrames pins = go 1 pins
+    where
+        go 10 [x, y]
+            | x + y < 10 = Just [Open x y]
+            | otherwise = Nothing
+        go 10 [x, y, z]
+            | x == 10 = Just [Strike y z]
+            | x + y == 10 = Just [Spare x z]
+            | otherwise = Nothing
+        go n (x:y:z:ys)
+            | x == 10 = fmap (Strike y z :) $ go (n+1) (y:z:ys)
+            | x + y == 10 = fmap (Spare x z :) $ go (n+1) (z:ys)
+            | x + y < 10 = fmap (Open x y :) $ go (n+1) (z:ys)
+            | otherwise = Nothing
+        go _ _ = Nothing
+
+frameScore :: Frame -> Int
+frameScore (Open x y) = x + y
+frameScore (Spare _ y) = 10 + y
+frameScore (Strike x y) = 10 + x + y
+
+score :: [Frame] -> Int
+score frames = sum $ map frameScore frames
