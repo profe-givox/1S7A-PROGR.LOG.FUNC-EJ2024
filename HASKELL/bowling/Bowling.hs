f036052a26b6de7be165cd3fd1753d833a29ec53
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-21 09:20:06 -0600

    bowling finalizado

--- HASKELL/bowling/Bowling.hs
+++ HASKELL/bowling/Bowling.hs
@@ -19,8 +19,25 @@ data Frame = Open Int Int
 
 
 
+-- toFrames :: [Int] -> Maybe [Frame]
+-- toFrames pins = go 1 pins
+--     where
+--         go 10 [x, y]
+--             | x + y < 10 = Just [Open x y]
+--             | otherwise = Nothing
+--         go 10 [x, y, z]
+--             | x == 10 = Just [Strike y z]
+--             | x + y == 10 = Just [Spare x z]
+--             | otherwise = Nothing
+--         go n (x:y:z:ys)
+--             | x == 10 = fmap (Strike y z :) $ go (n+1) (y:z:ys)
+--             | x + y == 10 = fmap (Spare x z :) $ go (n+1) (z:ys)
+--             | x + y < 10 = fmap (Open x y :) $ go (n+1) (z:ys)
+--             | otherwise = Nothing
+--         go _ _ = Nothing
+
 toFrames :: [Int] -> Maybe [Frame]
-toFrames pins = go 1 pins
+toFrames = go 1
     where
         go 10 [x, y]
             | x + y < 10 = Just [Open x y]
@@ -30,11 +47,12 @@ toFrames pins = go 1 pins
             | x + y == 10 = Just [Spare x z]
             | otherwise = Nothing
         go n (x:y:z:ys)
-            | x == 10 = fmap (Strike y z :) $ go (n+1) (y:z:ys)
-            | x + y == 10 = fmap (Spare x z :) $ go (n+1) (z:ys)
-            | x + y < 10 = fmap (Open x y :) $ go (n+1) (z:ys)
+            | x == 10 = (Strike y z :) <$> go (n+1) (y:z:ys)
+            | x + y == 10 = (Spare x z :) <$> go (n+1) (z:ys)
+            | x + y < 10 = (Open x y :) <$> go (n+1) (z:ys)
             | otherwise = Nothing
         go _ _ = Nothing
+
 
 frameScore :: Frame -> Int
 frameScore (Open x y) = x + y
@@ -42,4 +60,15 @@ frameScore (Spare _ y) = 10 + y
 frameScore (Strike x y) = 10 + x + y
 
 score :: [Frame] -> Int
-score frames = sum $ map frameScore frames
+score = sum . map frameScore
+
+scorePlay :: [Int] -> Maybe Int
+scorePlay pins = do
+            frames <- toFrames pins 
+            return $ score frames
+
+sumatorio :: [Int] -> Int
+-- sumatorio (x:xs) = x + sumatorio xs
+-- sumatorio [] = 0
+sumatorio xs = foldl (+) 0 xs
+
